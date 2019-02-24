69834a9f0bacf3f71e796ad9a4da0cb9590bf238
dieggsy <dieggsy@protonmail.com>
2019-02-24 14:59:11 -0500

    Add quadratic-complex-solutions
    - Also switch to potentially more numerically stable quadratic formulas
    - Closes #21

--- math-lib/math/private/number-theory/quadratic.rkt
+++ math-lib/math/private/number-theory/quadratic.rkt
@@ -3,28 +3,32 @@
 (require racket/list
          "types.rkt")
 
-(provide quadratic-solutions
+(provide quadratic-complex-solutions
+         quadratic-solutions
          quadratic-integer-solutions
          quadratic-natural-solutions)
 
-(: quadratic-solutions : Real Real Real -> (Listof Real))
-(define (quadratic-solutions a b c)
+(: quadratic-complex-solutions  : Complex Complex Complex -> (Listof Complex))
+(define (quadratic-complex-solutions a b c)
   ; return list of solutions to a a x^2 + b x + c = 0
   (let ([d (- (* b b) (* 4 a c))])
-    (cond
-      [(< d 0) '()]
-      [(= d 0) (list (/ b (* -2 a)))]
-      [else
-       (let ([sqrt-d (sqrt d)])
-         (list (/ (- (- b) sqrt-d) (* 2 a))
-               (/ (+ (- b) sqrt-d) (* 2 a))))])))
+    (if (= d 0)
+        (make-list 2 (/ b (* -2 a)))
+        (let* ([sqrt-d (sqrt d)]
+               [sign (if (>= 0 (real-part (* (conjugate b) sqrt-d))) 1 -1)]
+               [q (/ (+ b (* sign sqrt-d)) -2)])
+          (list (/ q a) (/ c q))))))
 
-(: quadratic-integer-solutions : Real Real Real -> (Listof Integer))
+(: quadratic-solutions : Complex Complex Complex -> (Listof Real))
+(define (quadratic-solutions a b c)
+  (filter real? (quadratic-solutions a b c)))
+
+(: quadratic-integer-solutions : Complex Complex Complex -> (Listof Integer))
 (define (quadratic-integer-solutions a b c)
   ; return list of integer solutions to a x^2 + b x + c = 0
   (filter exact-integer? (quadratic-solutions a b c)))
 
-(: quadratic-natural-solutions : Real Real Real -> (Listof Natural))
+(: quadratic-natural-solutions : Complex Complex Complex -> (Listof Natural))
 (define (quadratic-natural-solutions a b c)
   ; return list of nonnegative-integer solutions to a x^2 + b x + c = 0
   (filter natural? (quadratic-solutions a b c)))
