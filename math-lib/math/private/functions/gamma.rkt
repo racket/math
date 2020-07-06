db737eb3f1cf91e498d4fb17519d4148dbda5c06
bdeket <bedeke@hotmail.com>
2020-07-07 00:53:27 +0200

    complex-gamma: improve accuracy and sign for large (magnitude of) z

--- math-lib/math/private/functions/gamma.rkt
+++ math-lib/math/private/functions/gamma.rkt
@@ -273,26 +273,53 @@ Approximations:
                   [else  (flgamma-lanczos x)])))]))
 
 (define √2π 2.5066282746310005024157652848110)
-(: complex-gamma (Number -> Number))
+(: complex-gamma (Float-Complex -> Float-Complex))
 (define (complex-gamma z+1)
-  (define neg? (< (real-part z+1)0))
-  (define z (- (if neg? (- z+1) z+1) 1))
   (cond
-    [(or (= z 1)(= z 0))1]
+    [(or (= z+1 2)(= z+1 1)) 1.+0.i]
     [else
+     (define neg? (< (real-part z+1)0))
+     (define z (- (if neg? (- z+1) z+1) 1))
      (define zh (+ z 0.5))
      (define zgh (+ zh lanczos-complex-g))
      (define zp (expt zgh (* 0.5 zh)))
+     (define zzz (* zp (exp (- zgh)) zp))
 
-     (define ans
-       (* zp (exp (- zgh)) zp
-          √2π (+ (car lanczos-complex-c)
-                 (for/sum : Number ([a (in-list (cdr lanczos-complex-c))]
-                                    [i (in-naturals 1)])
-                   (/ a (+ z i))))))
-     (if neg?
-         (- (/ pi (* ans z+1 (sin (* pi z+1)))))
-         ans)]))
+     (define m
+       (+ (car lanczos-complex-c)
+          (for/fold ([s : Float-Complex 0.0+0.0i])
+                    ([a (in-list (cdr lanczos-complex-c))]
+                     [i (in-naturals 1)])
+            (+ s (/ a (+ z i))))))
+
+     ;(println m)(println zp)(println zgh)(println zzz)
+     (define (mm [z : Float-Complex])(or (infinite? (real-part z))(infinite? (imag-part z))))
+     (cond
+       [(or (mm zh)(mm zgh)) +nan.0+nan.0i]
+       [(or (mm zp)(mm zzz))
+        (cond
+          [neg? 0.0+0.0i]
+          [else
+           (define ans (* (* √2π m)
+                          (make-polar 1 (+ (* (magnitude zgh) (sin (angle zgh)))
+                                           (* 2 (angle zgh) (magnitude zh) (sin (+ (* .5 pi) (angle zh)))))) ))
+           (define α (angle ans))
+           (cond
+             [(< 0.0        α (* 0.5 pi))      +inf.0+inf.0i]
+             [(< (* 0.5 pi) α pi)              -inf.0+inf.0i]
+             [(< (- pi)     α (* -.5 pi))      -inf.0-inf.0i]
+             [(< (* -.5 pi) α 0.0)             +inf.0-inf.0i]
+             [(= 0.0 α)                        +inf.0+0.0i]
+             [(= (* 0.5 pi) α)                 0.0+inf.0i]
+             [(or (= pi α)(= (- pi) α))        -inf.0+0.0i]
+             [(= (* -.5 pi) α)                 0.0-inf.0i]
+             [else (error "complex-gamma: angle outside of range [-π , π]")])])]
+       ;[(mm m) (error "todo")]
+       [else
+        (define ans (* (* √2π m) zzz))
+        (if neg?
+            (/ (- pi) ans z+1 (sin (* pi z+1)))
+            ans)])]))
 
 (: gamma (case-> (One -> One)
                  (Integer -> Positive-Integer)
@@ -306,4 +333,4 @@ Approximations:
          (cond [(x . > . 0)  (factorial (- x 1))]
                [else  (raise-argument-error 'gamma "Real, not Zero or Negative-Integer" x)])]
         [(real? x)  (flgamma (fl x))]
-        [else (complex-gamma z)]))
+        [else (complex-gamma (number->float-complex z))]))
