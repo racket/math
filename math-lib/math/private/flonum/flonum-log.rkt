9665f406584e22a58e423d3fe485677c2f89bcbf
bdeket <bedeke@hotmail.com>
2020-09-15 16:08:20 +0200

    log1p: reduce fl2 functions

--- math-lib/math/private/flonum/flonum-log.rkt
+++ math-lib/math/private/flonum/flonum-log.rkt
@@ -23,51 +23,54 @@
   ;; Computes the value of log(1+x) in a way that is accurate for small x
   (define fllog1p
     (let ([expm1-min 0.0005])
-      (define (taylor-1to5 [x : Flonum]) : Flonum
-        (fl* x (fl+ 0.5 (fl* x (fl+ #i1/6 (fl* x (fl+ #i1/24 (fl* x #i1/120))))))))
-      (define taylor8 (make-flpolyfun (1.0 0.5 #i1/6 #i1/24 #i1/120 #i1/720 #i1/5040 #i1/40320)))
-      (define taylor (λ ([x : Flonum]) (* x (taylor8 x))))
-      (define (flexpm1/error [x : Flonum]) : (Values Flonum Flonum)
-        (cond
-          [((flabs x) . fl< . expm1-min)
-           ;; Taylor, first term in double-double precision - near zero - 5 terms
-           (let*-values ([(x-hi x-lo)  (flsplit x)]
-                         [(y2)     (taylor-1to5 x)]
-                         [(y2 y1)  (fl+/error y2 1.0)])
-             (fl2*split-fl y2 y1 x-hi x-lo))]
-          [else
-           (let*-values ([(x2 d2 d1)  (flexpm1-reduction x)]
-                         [(r2) (taylor x2)];we only need the upper part
-                         [(w2 w1)  (fl+/error r2 1.0)]
-                         [(y2 y1)  (fl2* d2 d1 w2 w1)])
-             (fl2+ y2 y1 r2))]))
-      (λ (x)
-        (define ax (flabs x))
-        (cond
-          [(fl= x -1.0) -inf.0]
-          [(fl< x -1.0) +nan.0]
-          [(flinfinite? x) +inf.0]
-          [(ax . fl>= . 4.0)  (fllog (fl+ 1.0 x))] ;; <= change boundary
-          [(ax . fl<= . (fl* 0.5 epsilon.0)) x]
-          [(x  . fl<= . -0.5)
-           (define y (fl+ 1.0 x))
-           (fl- (fllog y) (fl/ (fl- (fl- y 1.0) x) y))]
-          ;; calculate in fl2
-          [else
-           (define x+1 (fl+ 1.0 x))
-           (define-values (y2 y1)
-             ;; Gutted fl2log, to not do more fl2 computations than necessary
-             (let*-values
-                 ;; Estimate log(x) and do a Newton iteration using expm1
-                 ([(y)       (fllog x+1)]
-                  [(x*)      (fl- x+1 1.0)];= x
-                  [(z2 z1)   (flexpm1/error y)];; => expensive!
-                  [(w2)      (fl+ z2 1.0)]
-                  [(dy2)     (fl- (fl- x* z2) z1)]
-                  [(dy2 dy1) (fl//error dy2 w2)]
-                  [(v1 v2)   (fl+/error y dy2)])
-               (values v1 (fl+ v2 dy1))))
-           (fl- y2 (fl- (fl/ (fl- (fl- x+1 1.0) x) x+1) y1))]))))
+    ;; Taylor approximation for expm1 of 8 terms
+    (define taylor8 (make-flpolyfun (1.0 0.5 #i1/6 #i1/24 #i1/120 #i1/720 #i1/5040 #i1/40320)))
+    (define taylor (λ ([x : Flonum]) (* x (taylor8 x))))
+    (λ (x)
+      (define ax (flabs x))
+      (cond
+        [(fl= x -1.0) -inf.0]
+        [(fl< x -1.0) +nan.0]
+        [(flinfinite? x) +inf.0]
+        [(ax . fl>= . 4.0)  (fllog (fl+ 1.0 x))] ;; <= change boundary
+        [(ax . fl<= . (fl* 0.5 epsilon.0)) x]
+        [(x  . fl<= . -0.5)
+         (define y (fl+ 1.0 x))
+         (fl- (fllog y) (fl/ (fl- (fl- y 1.0) x) y))]
+        ;; calculate in fl2
+        [(ax . fl< . expm1-min)
+         ;; Gutted fl2log, to not do more fl2 computations than necessary
+         (let*-values
+             ([(x+1)      (fl+ x 1.0)]
+              [(x*)       (fl- x+1 1.0)]
+              ;; do a Newton iteration using expm1
+              ;; 0+y+d is a Taylor approximation for expm1, d=terms 2 to 5
+              [(y)        (fllog x+1)]
+              [(d) (fl* y (fl* y (fl+ 0.5 (fl* y (fl+ #i1/6 (fl* y (fl+ #i1/24 (fl* y #i1/120))))))))]
+              ;; fast-fl//error is ok since both arguments are expected in range [1e-35 1e2]
+              [(dy2 dy1)  (fast-fl//error (fl- (fl- x* y) d)
+                                          (fl+ (fl+ y d) 1.0))]
+              ;; fast-mono-fl+/error is ok since |y| > |dy2|
+              [(y2 y1)    (fast-mono-fl+/error y dy2)])
+           (fl- y2 (fl- (fl/ (fl- x* x) x+1) (fl+ y1 dy1))))]
+        [else
+         ;; Gutted fl2log, to not do more fl2 computations than necessary
+         (let*-values
+             ;; Estimate log(x) and do a Newton iteration using expm1
+             ([(x+1)      (fl+ x 1.0)]
+              [(x*)       (fl- x+1 1.0)]
+              [(y)        (fllog x+1)]
+              [(x2 d2 d1) (flexpm1-reduction y)]
+              [(r2)       (taylor x2)];
+              [(w2 w1)    (fast-mono-fl+/error 1.0 r2)]
+              ;; faster than fl2* and accurate 'enough'
+              [(u2 u1)    (fast-fl*/error d2 w2)]
+              [(z2 z1)    (fast-mono-fl+/error u2 r2)]
+              [(dy2 dy1)  (fast-fl//error (fl- (fl- x* z2)
+                                               (fl+ z1 (fl+ (fl+ u1 (fl* w2 d1)) (fl* d2 w1))))
+                                          (fl+ z2 1.0))]
+              [(y2 y1)    (fast-mono-fl+/error y dy2)])
+           (fl- y2 (fl- (fl/ (fl- x* x) x+1) (fl+ y1 dy1))))]))))
   
   (: fllog+ (Flonum Flonum -> Flonum))
   ;; Computes log(a+b) in a way that is accurate for a+b near 1.0
