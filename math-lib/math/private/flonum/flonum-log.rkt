dcd2ea1893dc5b45b26c8312997917a15fcd1c4a
bdeket <bedeke@hotmail.com>
2020-10-05 15:00:33 +0200

    remap

--- math-lib/math/private/flonum/flonum-log.rkt
+++ math-lib/math/private/flonum/flonum-log.rkt
@@ -22,50 +22,81 @@
   (: fllog1p (Float -> Float))
   ;; Computes the value of log(1+x) in a way that is accurate for small x
   (define fllog1p
-    (let ([epsilon.0/2 (fl* 0.5 epsilon.0)])
-      ;; Taylor approximation for log1p.
-      ;; don't put the first term (1.0) in the poly since to much accuracy will be lost
-      (define taylor-log1p-7terms
-        (let ([taylor (make-flpolyfun (#i-1/2 #i1/3 #i-1/4 #i1/5 #i-1/6 #i1/7))])
-          (λ ([x : Flonum]) (fl+ x (fl* x (fl* x (taylor x)))))))
-      (define taylor-log1p-16terms
-        (let ([taylor (make-flpolyfun (#i-1/2 #i1/3 #i-1/4 #i1/5 #i-1/6 #i1/7 #i-1/8 #i1/9
-                                       #i-1/10 #i1/11 #i-1/12 #i1/13 #i-1/14 #i1/15 #i-1/16))])
-          (λ ([x : Flonum]) (fl+ x (fl* x (fl* x (taylor x)))))))
-    ;; Taylor approximation for expm1 of 8 terms
-    (define taylor-expm1-8terms
-      (let ([taylor (make-flpolyfun (1.0 0.5 #i1/6 #i1/24 #i1/120 #i1/720 #i1/5040 #i1/40320))])
-        (λ ([x : Flonum]) (* x (taylor x)))))
+  (let ([epsilon.0/2 (fl* 0.5 epsilon.0)])
+    ;; approximation for log1p around 0.
+    ;; don't put the first term (1.0) in the poly since too much accuracy will be lost
+    (define taylor-0-to-1e-3
+      (let ([taylor (make-flpolyfun (#i-1/2 #i1/3 #i-1/4 #i1/5 #i-1/6 #i1/7))])
+        (λ ([x : Flonum]) (fl+ x (fl* x (fl* x (taylor x)))))))
+    (define taylor-0-to-1e-1
+      (let ([taylor (make-flpolyfun (#i-1/2 #i1/3 #i-1/4 #i1/5 #i-1/6 #i1/7 #i-1/8 #i1/9
+                                            #i-1/10 #i1/11 #i-1/12 #i1/13 #i-1/14 #i1/15 #i-1/16))])
+        (λ ([x : Flonum]) (fl+ x (fl* x (fl* x (taylor x)))))))
+
+    (define (normal [x : Flonum])
+      (define y (fl+ 1.0 x))
+      (fl- (fllog y) (fl/ (fl- (fl- y 1.0) x) y)))
+
+    ;; remap (log1p x) to (+ (log n) (log1p (/ (- x (- n 1)) n)))
+    ;; f2/f1 = (fl2 (bigfloat->real (bflog (bf n))))
+    (define (make-remap [n : Flonum][f2 : Flonum][f1 : Flonum])
+      (define m (- n 1))
+      (λ ([x : Flonum])
+        (define x* (fl/ (fl- x m) n))
+        (define ax* (flabs x*))
+        (define y (if (ax* . fl<= . epsilon.0/2)
+                      x*
+                      (if (ax* . fl<= . 1e-3)
+                          (taylor-0-to-1e-3 x*)
+                          (taylor-0-to-1e-1 x*))))
+        (fl+ f2 (fl+ y f1))))
+    
+    (define remap-0 (make-remap 0.88671875 -0.1202274269981598   2.8375497328444e-18))
+    (define remap-1 (make-remap 0.828125   -0.18859116980755003  7.432164219196925e-18))
+    (define remap-2 (make-remap 0.7734375  -0.2569104137850272  -2.502843296152504e-17))
+    (define remap-3 (make-remap 0.69140625 -0.36902771190573336  2.4362468710901017e-17))
+    (define remap-4 (make-remap 0.625      -0.4700036292457356   2.3229412495470032e-17))
+
+    (define remap+0 (make-remap 1.125  0.11778303565638346 -1.1971685747593677e-18))
+    (define remap+1 (make-remap 1.1875 0.17185025692665923 -6.0224538210113705e-18))
+    (define remap+2 (make-remap 1.25   0.22314355131420976 -9.091270597324799e-18))
+    (define remap+3 (make-remap 1.375  0.3184537311185346   2.7114779367326236e-17))
+    (define remap+4 (make-remap 1.5    0.4054651081081644  -2.8811380259626426e-18))
+    (define remap+5 (make-remap 1.75   0.5596157879354227   2.685492580212308e-17))
+    (define remap+6 (make-remap 2.0    0.6931471805599453   2.3190468138462996e-17))
+    (define remap+7 (make-remap 2.5    0.9162907318741551  -4.141195369011963e-17))
+    (define remap+8 (make-remap 3.0    1.0986122886681098  -9.07129723500153e-17))
+    (define remap+9 (make-remap 3.5    1.252762968495368   -6.097690852192957e-17))
+    (define remap+a (make-remap 4.4375 1.490091154801534    9.349202385763595e-17))
+    
     (λ (x)
       (define ax (flabs x))
       (cond
         [(ax . fl>= . 4.0)  (fllog (fl+ 1.0 x))] ;; <= change boundary
         [(ax . fl<= . epsilon.0/2) x]
-        [(ax . fl<= . 1e-3) (taylor-log1p-7terms x)]
-        [(ax . fl<= . 1e-1) (taylor-log1p-16terms x)]
+        [(ax . fl<= . 1e-3) (taylor-0-to-1e-3 x)]
+        [(ax . fl<= . 1e-1) (taylor-0-to-1e-1 x)]
         [(x  . fl=  . -1.0) -inf.0]
-        [(x  . fl<= . -0.5)
-         (define y (fl+ 1.0 x))
-         (fl- (fllog y) (fl/ (fl- (fl- y 1.0) x) y))]
-        ;; calculate in fl2
-        [else
-         ;; Gutted fl2log, to not do more fl2 computations than necessary
-         (let*-values
-             ;; Estimate log(x) and do a Newton iteration using expm1
-             ([(x+1)      (fl+ x 1.0)]
-              [(x*)       (fl- x+1 1.0)]
-              [(y)        (fllog x+1)]
-              [(x2 d2 d1) (flexpm1-reduction y)]
-              [(r2)       (taylor-expm1-8terms x2)];
-              [(w2 w1)    (fast-mono-fl+/error 1.0 r2)]
-              ;; faster than fl2* and accurate 'enough'
-              [(u2 u1)    (fast-fl*/error d2 w2)]
-              [(z2 z1)    (fast-mono-fl+/error u2 r2)]
-              [(dy2 dy1)  (fast-fl//error (fl- (fl- x* z2)
-                                               (fl+ z1 (fl+ (fl+ u1 (fl* w2 d1)) (fl* d2 w1))))
-                                          (fl+ z2 1.0))]
-              [(y2 y1)    (fast-mono-fl+/error y dy2)])
-           (fl- y2 (fl- (fl/ (fl- x* x) x+1) (fl+ y1 dy1))))]))))
+
+        [(x . fl<= . -0.45)      (normal x)]  ; map at / upper limit
+        [(x . fl<= . -0.3515625) (remap-4 x)] ; #x0.a0 / #x-0.5a
+        [(x . fl<= . -0.2734375) (remap-3 x)] ; #x0.b1 / #x-0.46
+        [(x . fl<= . -0.203125)  (remap-2 x)] ; #x0.c6 / #x-0.34
+        [(x . fl<= . -0.140625)  (remap-1 x)] ; #x0.d4 / #x-0.24
+        [(x . fl<= .  0.0)       (remap-0 x)] ; #x0.e3 / _
+
+        [(x . fl<= .  #x0.28) (remap+0 x)] ; #x1.2 / #x0.28
+        [(x . fl<= .  #x0.38) (remap+1 x)] ; #x1.3 / #x0.38
+        [(x . fl<= .  #x0.50) (remap+2 x)] ; #x1.4 / #x0.5
+        [(x . fl<= .  #x0.70) (remap+3 x)] ; #x1.6 / #x0.7
+        [(x . fl<= .  #x0.a0) (remap+4 x)] ; #x1.8 / #x0.a
+        [(x . fl<= .  #x0.e0) (remap+5 x)] ; #x1.c / #x0.e
+        [(x . fl<= .  #x1.40) (remap+6 x)] ; #x2.0 / #x1.4
+        [(x . fl<= .  #x1.c0) (remap+7 x)] ; #x2.8 / #x1.c
+        [(x . fl<= .  #x2.40) (remap+8 x)] ; #x3.0 / #x2.4
+        [(x . fl<= .  #x2.f0) (remap+9 x)] ; #x3.8 / #2.f0
+        [else                 (remap+a x)] ; #x4.4 / #4.0
+        ))))
   
   (: fllog+ (Flonum Flonum -> Flonum))
   ;; Computes log(a+b) in a way that is accurate for a+b near 1.0
