b9850b407a92f3a741e5692cfda23ffffd28af06
Matthew Flatt <mflatt@racket-lang.org>
2020-12-04 07:10:51 -0700

    add AArch64 Mac OS dependency

--- math-lib/info.rkt
+++ math-lib/info.rkt
@@ -9,6 +9,7 @@
                ("math-i386-macosx" #:platform "i386-macosx")
                ("math-x86_64-macosx" #:platform "x86_64-macosx")
                ("math-ppc-macosx" #:platform "ppc-macosx")
+               ("math-aarch64-macosx" #:platform "aarch64-macosx")
                ("math-win32-i386" #:platform "win32\\i386")
                ("math-win32-x86_64" #:platform "win32\\x86_64")
                ("math-x86_64-linux-natipkg" #:platform "x86_64-linux-natipkg")))
