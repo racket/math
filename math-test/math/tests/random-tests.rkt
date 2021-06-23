1d338108bcdacfe4313dc0118dace82e55e63342
Pavel Panchekha <me@pavpanchekha.com>
2021-06-22 18:05:04 -0600

    Fixes from testing

--- math-test/math/tests/random-tests.rkt
+++ math-test/math/tests/random-tests.rkt
@@ -1,4 +1,5 @@
 #lang typed/racket
+(require math/base)
 (require typed/rackunit)
 
 (check-true
@@ -6,9 +7,9 @@
     (> (random-natural (expt 2 64)) 4294967087)))
 
 (for ([n (in-range 100)])
-  (define x (random-natural 1 100))
+  (define x (random-integer 1 100))
   (check-true (and (<= 1 x) (< x 100))))
 
 (for ([n (in-range 100)])
   (define x (random-bits n))
-  (check-true (<= 0 x) (< x (expt 2 n))))
+  (check-true (and (<= 0 x) (< x (expt 2 n)))))
